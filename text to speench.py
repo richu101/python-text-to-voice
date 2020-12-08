5c56cf6e949dfe8f444f4d389e9dfdf54ffa0360
richu101 <richubini101@gmail.com>
2020-12-08 03:43:15 +0000

    python text to speech

--- text to speench.py
+++ text to speench.py
@@ -1,20 +1,28 @@
-import io
-import pygame
+
+from io import BytesIO
 from gtts import gTTS
+from playsound import playsound
 
 def speak(text):
-        with io.BytesIO() as file:
-            gTTS(text=text, lang="en").write_to_fp(file)
-            file.seek(0)
-            # play the sound
-            pygame.mixer.init()
-            pygame.mixer.music.load(file)
-            pygame.mixer.music.play()
-            while pygame.mixer.music.get_busy():
-                continue
+        mpe_fp = BytesIO()
+        tts = gTTS(text,lang='en')
+        tts.save('mp3.wav')
+      #  tts.write_to_fp(mpe_fp)
+      # playsound(mpe_fp)
+
+
 
 if __name__ == '__main__' :
     print("What should i say..?")
     text = input("  > >  ")
     speak(text)
 
+"""
+
+
+from pydub import AudioSegment
+from pydub.playback import play
+
+song = AudioSegment.from_wav("mp3.mp3")
+play(song)
+"""
