a004b1523b19a8f9616d24a1bc1e0e49e469913e
richu101 <richubini101@gmail.com>
2020-12-10 06:52:44 +0000

    volume control added

--- mp3 player.py
+++ mp3 player.py
@@ -1,58 +1,55 @@
-from pygame import mixer 
+from pygame import mixer
 
 i = 0.5
-# Starting the mixer 
-mixer.init() 
+# Starting the mixer
+mixer.init()
 
-# Loading the song 
-mixer.music.load("hello.mp3") 
+# Loading the song
+mixer.music.load("1ds.mp3")
 
-# Setting the volume 
-mixer.music.set_volume(0.7) 
+# Setting the volume
+mixer.music.set_volume(0.7)
 
-# Start playing the song 
-mixer.music.play() 
+# Start playing the song
+mixer.music.play()
 
-# infinite loop 
-while True: 
-	
-	print("Press 'p' to pause, 'r' to resume") 
-	print("Press 'e' to exit the program") 
-	query = input(" ") 
-	
-	if query == 'p': 
+# infinite loop
+while True:
 
-		# Pausing the music 
-		mixer.music.pause()	 
-	elif query == 'r': 
+    print("Press 'p' to pause, 'r' to resume")
+    print("Press 'e' to exit the program")
+    query = input(" ")
 
-		# Resuming the music 
-		mixer.music.unpause() 
-        
-    # Volume control
-    elif querry == 'v'
-        volin = input(" Press -> arrow to volume UP and <- Arrow to volume down")
+    if query == 'p':
+
+        # Pausing the music
+        mixer.music.pause()
+    elif query == 'r':
+
+        # Resuming the music
+        mixer.music.unpause() 
+    elif query == 'e': 
+
+        # Stop the mixer 
+        mixer.music.stop() 
+        break
+
+    elif query == 'v':   
+        # Volume control
+        volin = input(" <- Arrow key to volume down & - -> Arrow key to volume up ")
         if volin == "^[[C":            
             i += .1
+            print("volume =")
+            print(i)
             mixer.music.set_volume(i) 
             if i >= 1 :
                 i == .9
                 print("You are at max volume")
-        elif volin == "^[[C":            
+        elif volin == "^[[D":            
             i -= .1
+            print("volume =")
+            print(i)
             mixer.music.set_volume(i) 
             if i <= 0 :
-                i == .9
+                i == .1
                 print("You are at min volume")
-    # mixer.music.play() 
-	elif query == 'e': 
-
-		# Stop the mixer 
-		mixer.music.stop() 
-		break
-
-
-
-
-^[[D
-
