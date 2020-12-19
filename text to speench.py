47608c5158f881ebd769be428cf8b79483de46cc
unknown <angelamgprofessional@gmail.com>
2020-12-19 19:37:06 +0530

    Update

--- text to speench.py
+++ text to speench.py
@@ -1,49 +1,68 @@
 from pygame import mixer
 from gtts import gTTS
 
-#text to speech
-text='hello'                
-tts = gTTS(text, lang='en') 
-tts.save('file.mp3')
-
 mixer.init()
-
 voice=r'file.mp3'
-# Calling the file to voice 
 
-def play_music(voice):
+def tempfile():                                 
+    tts = gTTS('hello', lang='en')
+    tts.save('hello.mp3')
+    return 'hello.mp3'
+
+def play_music(voice,vol=0.7):
    
     """
-    This function is to play the drum audios in the drum sound audios
+    This function is to play the audios file
     """
     mixer.music.load(voice)
 
     # Setting the volume
-    mixer.music.set_volume(0.7)
+    mixer.music.set_volume(vol)
 
     # Start playing the song
     mixer.music.play()
 
+def  menu():
+    play_music(voice)
+
+    #infinite loop
+    while True:
+        
+        print("Press 'p' to pause, 'r' to resume the speech")
+        print("Press 'e' to exit the program")
+        query = input(" ")
+    
+        if query == 'p':
+
+            # Pausing the music
+            mixer.music.pause()
+        elif query == 'r':
+
+            # Resuming the music
+            mixer.music.unpause()
+        elif query == 'e':
+
+            # Stop the mixer
+            mixer.music.stop()
+            break
+    
+    #temporary pass
+    play_music(tempfile(),0)
 
 # infinite loop
 while True:
 
-    print("Press 'p' to pause, 'r' to resume")
-    print("Press 'e' to exit the program")
-    play_music(voice)
-    query = input(" ")
+    text = input('Enter the text to be read: ')
     
-   
-    if query == 'p':
+    #Converts the user input text to speech
+    tts = gTTS(text, lang='en')
 
-        # Pausing the music
-        mixer.music.pause()
-    elif query == 'r':
-
-        # Resuming the music
-        mixer.music.unpause()
-    elif query == 'e':
-
-        # Stop the mixer
-        mixer.music.stop()
+    #saves as file.mp3
+    tts.save('file.mp3')
+    
+    menu()
+    ch=input("Would you like to convert another text (y/n)?...")
+    if ch == 'y' or ch == 'Y':
+        continue
+    else:
         break
